dfb0bdffbdac19c136632e429e2277a9007926ae
amirisback <faisalamircs@gmail.com>
2018-09-06 01:15:32 +0700

    Starter code and resource

---
+++ app/src/main/java/org/d3ifcool/jagosholat/TentangKamiActivity.java
@@ -0,0 +1,30 @@
+package org.d3ifcool.jagosholat;
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * 40-mobpro-4002-jago-sholat
+ * Copyright (C) 05/09/2018.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Line     : bullbee117
+ * Phone    : 081357108568
+ * Majors   : D3 Teknik Informatika 2016
+ * Campus   : Telkom University
+ * -----------------------------------------
+ * id.amirisback.frogobox
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TentangKamiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_kami);
+    }
+}
