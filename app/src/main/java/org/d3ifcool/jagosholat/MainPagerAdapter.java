1996461ed67cb26667cd96103ffda29b9514b216
bryanrafsanzai <bryanrafsanzani46@gmail.com>
2018-09-13 16:22:56 +0700

    1.2 Menambahkan fragment 4 fragment kosong, membuat model data

--- app/src/main/java/org/d3ifcool/jagosholat/MainPagerAdapter.java
+++ app/src/main/java/org/d3ifcool/jagosholat/MainPagerAdapter.java
@@ -1,17 +1,45 @@
 package org.d3ifcool.jagosholat;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
 /**
- * Created by ikhsan ramadhan
- * =========================================
- * 40-mobpro-4002-jago-sholat
- * Copyright (C) 9/10/2018.
- * All rights reserved
- * -----------------------------------------
- * Name     : Muhamad Ikhsan Ramadhan
- * E-mail   : ikhsanramadhan28@gmail.com
- * Majors   : D3 Teknik Informatika 2016
- * Campus   : Telkom University
- * -----------------------------------------
+ * Created by Faisal Amir on 25/02/2018.
  */
-public class MainPagerAdapter {
+
+public class MainPagerAdapter extends FragmentPagerAdapter {
+
+    private int NumberTab;
+
+    public MainPagerAdapter(FragmentManager fm, int numberTab) {
+        super(fm);
+        NumberTab = numberTab;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0 :
+                return new CatatanFragment();
+            case 1 :
+                return new JadwalFragment();
+            case 2 :
+                return new StatistikFragment();
+            case 3 :
+                return new KompasFragment();
+            case 4 :
+                return new FeatureFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return NumberTab;
+    }
 }
