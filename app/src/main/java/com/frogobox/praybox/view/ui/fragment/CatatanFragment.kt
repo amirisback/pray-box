b55a3e7cba678bb4c53ddfb523eefb36f815bc3c
amirisback <faisalamircs@gmail.com>
2019-12-15 15:14:47 +0700

    update: fix error

--- app/src/main/java/com/frogobox/praybox/view/ui/fragment/CatatanFragment.kt
+++ app/src/main/java/com/frogobox/praybox/view/ui/fragment/CatatanFragment.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import com.frogobox.praybox.R
 import com.frogobox.praybox.base.view.ui.BaseFragment
 import com.frogobox.praybox.source.local.DataOperation
@@ -41,19 +39,26 @@ class CatatanFragment : BaseFragment() {
     private val mHadistText = intArrayOf(R.string.hadis_text_0, R.string.hadis_text_1, R.string.hadis_text_2,
             R.string.hadis_text_3, R.string.hadis_text_4, R.string.hadis_text_5)
 
+    private lateinit var mDialogView : View
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? { // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_catatan, container, false)
+        mDialogView = inflater.inflate(R.layout.content_catatan_button, null)
+        return inflater.inflate(R.layout.fragment_catatan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val methodHelper = MethodHelper()
-        val waktuShalatHelper = WaktuShalatHelper()
+        val mWaktuHelper = WaktuShalatHelper()
         val crud = DataOperation()
 
         // Set tampilan tanggal dan waktu
         methodHelper.getSystemTime()
         methodHelper.getSystemRealTime()
         methodHelper.getSumRealTime()
-        waktuShalatHelper.setJadwalShalat(jadwal_textview_shalat)
+        mWaktuHelper.setJadwalShalat(tv_jadwal_shalat)
         catatan_textview_tanggal.text = methodHelper.dateToday
 
         // Set Data Random Hadist untuk XML Layout
@@ -69,19 +74,20 @@ class CatatanFragment : BaseFragment() {
 
         // Deklarasi Element XML Update View
         val mDialogBuilder = AlertDialog.Builder(context)
-        val mDialogView = inflater.inflate(R.layout.content_catatan_button, null)
+
 
         // Deklarasi custom dialog
         val mDialogForm = CatatanCustomDialog(mDialogBuilder, mDialogView,
-                methodHelper, context, crud, waktuShalatHelper)
+                methodHelper, context, crud, mWaktuHelper)
 
         // Fungsi untuk menampilkan button simpan
-        val mShalatNow = jadwal_textview_shalat.text.toString()
+        val mShalatNow = tv_jadwal_shalat.text.toString()
         mDialogForm.viewSaveButton(catatan_button_catat_ibadah, mShalatNow)
 
         // Panggil method untuk mencatat
         catatan_button_catat_ibadah.setOnClickListener { mDialogForm.DialogForm() }
 
-        return rootView
+
     }
+
 }
