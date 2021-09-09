8d8c195d8e5b20a710620aa279f253c8faedf954
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-09-09 11:30:22 +0700

    update: refactoring code

--- app/src/main/java/com/frogobox/praybox/model/DoaShalat.kt
+++ app/src/main/java/com/frogobox/praybox/model/DoaShalat.kt
@@ -3,4 +3,8 @@ package com.frogobox.praybox.model
 /**
  * Created by Bryan on 3/12/2018.
  */
-data class DoaShalat(var arabDoa: String, var latinDoa: String, var terjemahDoa: String)
+data class DoaShalat(
+    var arabDoa: String,
+    var latinDoa: String,
+    var terjemahDoa: String
+)
